2057f1474cc87c77d35494170bcb5c39746f5c26
omar faruk <69864169+whereisfarukk@users.noreply.github.com>
2025-05-10 22:43:11 +0600

    fix:click on title takes product page

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -39,7 +39,7 @@ const Product = ({ product }) => {
       </div>
       {/* {category & title & price} */}
       <div className="text-sm capitalize text-gray-500 mb-1">{category}</div>
-      <Link to={`/products/${id}`}>
+      <Link to={`/product/${id}`}>
         <h2 className="font-semibold mb-1">{title}</h2>
       </Link>
       <h2 className="font-semibold">$ {price}</h2>
